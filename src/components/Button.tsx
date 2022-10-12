f824ce328dda742a6784271a0b52d78de33c4e67
Juan Sebastian Valdes <jvaldes@robinfood.com>
2022-10-12 10:59:08 -0500

    add some new features

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
-import { FC } from 'react';
+import React from "react";
+import { FC } from "react";
 
 export type Props = {
   children: string;
+  label?: string;
+  type?: "button" | "submit" | "reset" | undefined;
 };
 
-export const Button: FC<Props> = ({ children }) => {
-  return <button className="bg-blue-ml rounded-md w-full text-white-ml p-2">{children}</button>;
+export const Button: FC<Props> = ({ children, label, type }) => {
+  return (
+    <button
+      className='bg-blue-ml rounded-md w-full text-white-ml p-2'
+      name={label}
+      type={type}
+    >
+      {children}
+    </button>
+  );
 };
--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,43 +1,45 @@
-import { FC, useState } from 'react';
-import Router from 'next/router';
+import { FC, useState } from "react";
+import Router from "next/router";
 
-import IconSearch from './icons/Search';
+import IconSearch from "./icons/Search";
 
 export const InputSearch: FC = () => {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const handleKeyInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       handleSearch();
     }
   };
 
   const handleSearch = (): void => {
-    if (search.trim().length > 0) {
+    const normalizeSearch = search.trim().toLocaleLowerCase();
+    if (normalizeSearch.length > 0) {
       Router.push({
-        pathname: '/items',
-        query: { q: search },
+        pathname: "/items",
+        query: { q: normalizeSearch },
       });
     } else {
       Router.push({
-        pathname: '/',
+        pathname: "/",
       });
     }
   };
 
   return (
-    <div className="ml-3 w-full flex flex-row">
+    <div className='ml-3 w-full flex flex-row'>
       <input
-        className="bg-white focus:outline-none focus:shadow-outline border-gray-300 w-full rounded-sm py-1 px-4 block appearance-none leading-normal font-light h-10"
-        type="text"
-        placeholder="Search"
+        className='bg-white focus:outline-none focus:shadow-outline border-gray-300 w-full rounded-sm py-1 px-4 block appearance-none leading-normal font-light h-10'
+        type='text'
+        placeholder='Search'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         onKeyPress={handleKeyInput}
       />
       <span
-        className="bg-grey-high-ml flex justify-center items-center cursor-pointer w-10 rounded-tr-sm rounded-br-sm"
+        className='bg-grey-high-ml flex justify-center items-center cursor-pointer w-10 rounded-tr-sm rounded-br-sm'
         onClick={() => handleSearch()}
+        data-testid={"button-icon-search"}
       >
         <IconSearch />
       </span>
